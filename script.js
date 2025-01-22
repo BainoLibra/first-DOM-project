ee0255e61051c2d89a7e962f0c150811ef6e9489
Bainomugisha Libra <148781031+BainoLibra@users.noreply.github.com>
2025-01-22 12:06:42 +0300

    updated

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 alert('test')
 let liElement=document.getElementById("skills")
-console.log(document.title)
+console.log(liElement.document.skills)
+document.body.style.backgroundColor="blue"
+const title = "About Libra";
+const welcommsg=document.getElementById("welcommesg");
+welcommsg.textContent+=title===""?'Guest':title;
